e423e6f27cf85b0d0f3b3f46dccaa3ca924ed56c
Temi-t <temiemmanuel@yahoo.com>
2020-09-14 15:43:57 +0100

    Updates

---
+++ precache-manifest.a4b856307aa67eccb1a933367625612e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c9794ffe771e672d6c6d5beffdbeea",
+    "url": "/jetflix/index.html"
+  },
+  {
+    "revision": "6be30e158ea671fcc6f4",
+    "url": "/jetflix/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab89cc1b9b38e6233386",
+    "url": "/jetflix/static/js/2.a4f90bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jetflix/static/js/2.a4f90bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be30e158ea671fcc6f4",
+    "url": "/jetflix/static/js/main.266886b8.chunk.js"
+  },
+  {
+    "revision": "6a6e7b31d7a10a1427d9",
+    "url": "/jetflix/static/js/runtime-main.6dba2a84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jetflix/static/media/logo.5d5d9eef.svg"
+  }
+]);
