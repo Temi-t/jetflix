f58f490e2b5c18161c348eef926ebb0dd27f62d9
Temi-t <temiemmanuel@yahoo.com>
2020-09-22 11:57:35 +0100

    syntax errors 😥

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import axios from './axios';
+// import axios from './axios';
+import axios from 'axios';
+import "./row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl }) {
 
@@ -8,16 +12,30 @@ function Row({ title, fetchUrl }) {
     useEffect(()=>{
         async function fetchData(){
             const request = await axios.get(fetchUrl)
-            console.log(request);
+            console.log(request.data.results);
+            setMovies(request.data.results)
             return request;
+            
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+    
 
     return(
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-            {/* container -> posters */}
+            <div className="row_posters">
+                {/*several row_posters*/}
+                {movies.map((movie,i) => (
+                    <img
+                        className="row_poster"
+                        key={'movie_'+i} 
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name} 
+                     />
+                ))}
+            </div>
+             {/* container -> posters */}
         </div>
     )
 }
--- src/requests.js
+++ src/requests.js
@@ -1,9 +1,21 @@
 const API_KEY = "f2556456b7857fa3a4ba41804346890e";
 
 
+// const requests ={
+//     fetchTrending: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+//     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=213`,
+//     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+//     fetcthActionMovies: `discover/movie?api_key=${API_KEY}$with_genres=28`,
+//     fetcthComedyMovies: `discover/movie?api_key=${API_KEY}$with_genres=35`,
+//     fetcthHorrorMovies: `discover/movie?api_key=${API_KEY}$with_genres=27`,
+//     fetcthRomanceMovies: `discover/movie?api_key=${API_KEY}$with_genres=10749`,
+//     fetcthDocumentaries: `discover/movie?api_key=${API_KEY}$with_genres=99`,
+// }
+
+
 const requests ={
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=213`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetcthActionMovies: `discover/movie?api_key=${API_KEY}$with_genres=28`,
     fetcthComedyMovies: `discover/movie?api_key=${API_KEY}$with_genres=35`,
@@ -13,3 +25,4 @@ const requests ={
 }
 
 export default requests;
+// https://api.themoviedb.org/3/trending/all/week?api_key=f2556456b7857fa3a4ba41804346890e$language=en-US
