93a46f43fe8a669f074c3ac39be3b655797f0af5
Temi-t <temiemmanuel@yahoo.com>
2020-09-29 19:47:15 +0100

    😼 Added trailers and new url 🎬 🎞  📽

--- src/Row.js
+++ src/Row.js
@@ -12,35 +12,41 @@ function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
     const [trailerUrl, setTrailerUrl] = useState("");
 
+    // useEffect(()=>{
+    //     ========
+    // });
+
+
     useEffect(()=>{
         async function fetchData(){
             const request = await axios.get(fetchUrl)
             // console.log(request.data.results);
             setMovies(request.data.results)
-            return request;
-            
+            return request; 
         }
+
         fetchData();
     }, [fetchUrl]);
     
 
-    {/*Recommended height:270 width: 480 for 16:9 players*/}
+    /*Recommended height:270 width: 480 for 16:9 players*/
     const opts = {
         height: '390', 
-        width: '100',
+        width: '100%',
         playerVars: {
             autoplay: 1,
         },
     };
 
-
+    //some the movie trailers cannot be found because there are no videos of them on youTube to be searched for
     const handleClick = (movie) =>{
         if(trailerUrl) {
             setTrailerUrl('');
         } else{
-            movieTrailer( movie?.name || '' )
+            movieTrailer( movie?.name || movie?.original_name || 'Sorry, Blame It On Me (Official Video)' || '' )
             .then(url => {
-                const urlParams = URLSearchParams(new URL(url).search);
+                // console.log(new URL(url).search)
+                const urlParams = new URLSearchParams(new URL(url).search);
                 setTrailerUrl(urlParams.get("v"));
             }).catch(error=> console.log(error))
         }
@@ -62,9 +68,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
                      />
                 ))}
             </div>
-             {trailerUrl && <YouTube videoId='QOD_ZsSc9Zc' opts={opts}   />}
+             {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
-    )
+    );
 }
 
 
--- src/requests.js
+++ src/requests.js
@@ -1,28 +1,15 @@
 const API_KEY = "f2556456b7857fa3a4ba41804346890e";
 
-
-// const requests ={
-//     fetchTrending: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
-//     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}$with_networks=213`,
-//     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-//     fetcthActionMovies: `discover/movie?api_key=${API_KEY}$with_genres=28`,
-//     fetcthComedyMovies: `discover/movie?api_key=${API_KEY}$with_genres=35`,
-//     fetcthHorrorMovies: `discover/movie?api_key=${API_KEY}$with_genres=27`,
-//     fetcthRomanceMovies: `discover/movie?api_key=${API_KEY}$with_genres=10749`,
-//     fetcthDocumentaries: `discover/movie?api_key=${API_KEY}$with_genres=99`,
-// }
-
-
 const requests ={
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
-    fetchActionMovies: `discover/movie?api_key=${API_KEY}`,
-    fetchComedyMovies: `discover/movie?api_key=${API_KEY}`,
-    fetchHorrorMovies: `discover/movie?api_key=${API_KEY}`,
-    fetchRomanceMovies: `discover/movie?api_key=${API_KEY}`,
-    fetchDocumentaries: `discover/movie?api_key=${API_KEY}`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
+
 export default requests;
-// https://api.themoviedb.org/3/trending/all/week?api_key=f2556456b7857fa3a4ba41804346890e$language=en-US
