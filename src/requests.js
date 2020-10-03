47d4fd15f0a8bb8ae5f704ce1f1724c5b2c7f287
Temi-t <temiemmanuel@yahoo.com>
2020-10-03 17:54:10 +0100

    🚀 favicon and new row 🏷

--- src/requests.js
+++ src/requests.js
@@ -9,6 +9,10 @@ const requests ={
     fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchAnime: `discover/movie?api_key=${API_KEY}&with_genres=16`,
+    // fetchTrial1: `discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1`
+    fetchTrial1: `discover/movie?api_key=${API_KEY}&with_genres=10749&language=en-US&include_adult=true&include_video=true`
+
 }
 
 
